fedc6b558df2553bb3e37db14aaf9b547d86f4ca
Marcus <77679714+nicsure@users.noreply.github.com>
2025-06-23 14:32:13 +0100

    Add project files.

---
+++ RT880-FWFlasher/UI.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace RT880_FWFlasher
+{
+    partial class UI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MainGrid = new TableLayoutPanel();
+            label1 = new Label();
+            FileNameLabel = new Label();
+            BrowseButton = new Button();
+            StatusPanel = new TableLayoutPanel();
+            StatusLabel = new Label();
+            StartButton = new Button();
+            AbortButton = new Button();
+            ProgressBar = new TrackBar();
+            ComPorts = new ComboBox();
+            MainGrid.SuspendLayout();
+            StatusPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ProgressBar).BeginInit();
+            SuspendLayout();
+            // 
+            // MainGrid
+            // 
+            MainGrid.BackColor = Color.Black;
+            MainGrid.ColumnCount = 3;
+            MainGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            MainGrid.ColumnStyles.Add(new ColumnStyle());
+            MainGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50.0000076F));
+            MainGrid.Controls.Add(label1, 0, 0);
+            MainGrid.Controls.Add(FileNameLabel, 0, 1);
+            MainGrid.Controls.Add(BrowseButton, 0, 2);
+            MainGrid.Controls.Add(StatusPanel, 0, 5);
+            MainGrid.Controls.Add(StartButton, 0, 3);
+            MainGrid.Controls.Add(AbortButton, 1, 3);
+            MainGrid.Controls.Add(ProgressBar, 0, 4);
+            MainGrid.Controls.Add(ComPorts, 2, 3);
+            MainGrid.Dock = DockStyle.Fill;
+            MainGrid.Location = new Point(0, 0);
+            MainGrid.Name = "MainGrid";
+            MainGrid.RowCount = 6;
+            MainGrid.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            MainGrid.RowStyles.Add(new RowStyle());
+            MainGrid.RowStyles.Add(new RowStyle());
+            MainGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            MainGrid.RowStyles.Add(new RowStyle(SizeType.Absolute, 70F));
+            MainGrid.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            MainGrid.Size = new Size(1066, 391);
+            MainGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            MainGrid.SetColumnSpan(label1, 3);
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(391, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(284, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Firmware Image File";
+            // 
+            // FileNameLabel
+            // 
+            FileNameLabel.AutoSize = true;
+            FileNameLabel.BackColor = Color.FromArgb(64, 0, 64);
+            FileNameLabel.BorderStyle = BorderStyle.FixedSingle;
+            MainGrid.SetColumnSpan(FileNameLabel, 3);
+            FileNameLabel.Dock = DockStyle.Fill;
+            FileNameLabel.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FileNameLabel.ForeColor = Color.White;
+            FileNameLabel.Location = new Point(15, 60);
+            FileNameLabel.Margin = new Padding(15, 10, 15, 0);
+            FileNameLabel.Name = "FileNameLabel";
+            FileNameLabel.Padding = new Padding(3);
+            FileNameLabel.Size = new Size(1036, 36);
+            FileNameLabel.TabIndex = 1;
+            FileNameLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // BrowseButton
+            // 
+            BrowseButton.Anchor = AnchorStyles.None;
+            BrowseButton.AutoSize = true;
+            BrowseButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BrowseButton.BackColor = Color.FromArgb(64, 64, 64);
+            MainGrid.SetColumnSpan(BrowseButton, 3);
+            BrowseButton.FlatStyle = FlatStyle.Flat;
+            BrowseButton.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BrowseButton.ForeColor = Color.White;
+            BrowseButton.Location = new Point(482, 101);
+            BrowseButton.Margin = new Padding(3, 5, 3, 3);
+            BrowseButton.Name = "BrowseButton";
+            BrowseButton.Size = new Size(102, 40);
+            BrowseButton.TabIndex = 2;
+            BrowseButton.Text = " Browse ";
+            BrowseButton.UseVisualStyleBackColor = false;
+            BrowseButton.Click += BrowseButton_Click;
+            // 
+            // StatusPanel
+            // 
+            StatusPanel.BackColor = Color.FromArgb(0, 0, 64);
+            StatusPanel.ColumnCount = 1;
+            MainGrid.SetColumnSpan(StatusPanel, 3);
+            StatusPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            StatusPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            StatusPanel.Controls.Add(StatusLabel, 0, 0);
+            StatusPanel.Dock = DockStyle.Fill;
+            StatusPanel.Location = new Point(3, 344);
+            StatusPanel.Name = "StatusPanel";
+            StatusPanel.RowCount = 1;
+            StatusPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            StatusPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            StatusPanel.Size = new Size(1060, 44);
+            StatusPanel.TabIndex = 0;
+            // 
+            // StatusLabel
+            // 
+            StatusLabel.AutoSize = true;
+            StatusLabel.Dock = DockStyle.Fill;
+            StatusLabel.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            StatusLabel.ForeColor = Color.White;
+            StatusLabel.Location = new Point(3, 0);
+            StatusLabel.Name = "StatusLabel";
+            StatusLabel.Size = new Size(1054, 44);
+            StatusLabel.TabIndex = 0;
+            StatusLabel.Text = "Ready";
+            StatusLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // StartButton
+            // 
+            StartButton.Anchor = AnchorStyles.Right;
+            StartButton.AutoSize = true;
+            StartButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            StartButton.BackColor = Color.FromArgb(0, 64, 0);
+            StartButton.Enabled = false;
+            StartButton.FlatStyle = FlatStyle.Flat;
+            StartButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            StartButton.ForeColor = Color.White;
+            StartButton.Location = new Point(376, 185);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(97, 44);
+            StartButton.TabIndex = 2;
+            StartButton.Text = " Flash ";
+            StartButton.UseVisualStyleBackColor = false;
+            StartButton.Click += StartButton_Click;
+            // 
+            // AbortButton
+            // 
+            AbortButton.Anchor = AnchorStyles.None;
+            AbortButton.AutoSize = true;
+            AbortButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            AbortButton.BackColor = Color.FromArgb(64, 0, 0);
+            AbortButton.Enabled = false;
+            AbortButton.FlatStyle = FlatStyle.Flat;
+            AbortButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AbortButton.ForeColor = Color.White;
+            AbortButton.Location = new Point(479, 185);
+            AbortButton.Name = "AbortButton";
+            AbortButton.Size = new Size(107, 44);
+            AbortButton.TabIndex = 2;
+            AbortButton.Text = " Abort ";
+            AbortButton.UseVisualStyleBackColor = false;
+            AbortButton.Click += AbortButton_Click;
+            // 
+            // ProgressBar
+            // 
+            MainGrid.SetColumnSpan(ProgressBar, 3);
+            ProgressBar.Dock = DockStyle.Fill;
+            ProgressBar.Enabled = false;
+            ProgressBar.Location = new Point(3, 274);
+            ProgressBar.Maximum = 100;
+            ProgressBar.Name = "ProgressBar";
+            ProgressBar.Size = new Size(1060, 64);
+            ProgressBar.TabIndex = 3;
+            ProgressBar.TickFrequency = 10;
+            ProgressBar.TickStyle = TickStyle.Both;
+            // 
+            // ComPorts
+            // 
+            ComPorts.Anchor = AnchorStyles.Left;
+            ComPorts.BackColor = Color.Black;
+            ComPorts.FlatStyle = FlatStyle.Flat;
+            ComPorts.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ComPorts.ForeColor = Color.White;
+            ComPorts.FormattingEnabled = true;
+            ComPorts.Location = new Point(592, 187);
+            ComPorts.Name = "ComPorts";
+            ComPorts.Size = new Size(147, 40);
+            ComPorts.TabIndex = 4;
+            ComPorts.DropDown += ComPorts_DropDown;
+            // 
+            // UI
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1066, 391);
+            Controls.Add(MainGrid);
+            Name = "UI";
+            Text = "RT-880 Firmware Flasher";
+            MainGrid.ResumeLayout(false);
+            MainGrid.PerformLayout();
+            StatusPanel.ResumeLayout(false);
+            StatusPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ProgressBar).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel MainGrid;
+        private TableLayoutPanel StatusPanel;
+        private Label StatusLabel;
+        private Label label1;
+        private Label FileNameLabel;
+        private Button BrowseButton;
+        private Button StartButton;
+        private Button AbortButton;
+        private TrackBar ProgressBar;
+        private ComboBox ComPorts;
+    }
+}
---
+++ RT880-FWFlasher/UI.cs
@@ -0,0 +1,209 @@
+using System.IO.Ports;
+
+namespace RT880_FWFlasher
+{
+    public partial class UI : Form
+    {
+        private SerialPort? flashingPort = null;
+
+        public UI()
+        {
+            InitializeComponent();
+            EnumerateSerialPorts();
+        }
+
+        public void EnumerateSerialPorts()
+        {
+            var selected = ComPorts.SelectedItem;
+            ComPorts.Items.Clear();
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                ComPorts.Items.Add(portName);
+            }
+            try { ComPorts.SelectedItem = selected; } catch { }
+            if (ComPorts.SelectedIndex == -1 && ComPorts.Items.Count > 0)
+                ComPorts.SelectedIndex = 0;
+        }
+
+        public static byte[] ConstructPacket(int type, int address, byte[] data, int offset, int count)
+        {
+            byte[] packet = new byte[count + 4];
+            int cs = packet.Length - 1;
+            packet[0] = (byte)type;
+            packet[1] = (byte)((address >> 8) & 0xff);
+            packet[2] = (byte)(address & 0xff);
+            packet[cs] = 0x52;
+            Array.Copy(data, offset, packet, 3, count);
+            for (int i = 0; i < cs; i++)
+            {
+                packet[cs] += packet[i];
+            }
+            return packet;
+        }
+
+        public static void ClosePort(SerialPort? port)
+        {
+            using (port)
+            {
+                port?.Close();
+            }
+        }
+
+        public static int ReadByte(SerialPort port)
+        {
+            try
+            {
+                return port.ReadByte();
+            }
+            catch (TimeoutException) { return -2; }
+            catch { return -1; }
+        }
+
+        public bool GetAck(SerialPort port)
+        {
+            switch (ReadByte(port))
+            {
+                case 6: return true;
+                case -1: SetStatus("COM Port Read Error/Abort"); break;
+                case -2: SetStatus("COM Port Timeout"); break;
+                default: SetStatus("Bad Acknowledgement"); break;
+            }
+            return false;
+        }
+
+        public bool WriteData(SerialPort port, byte[] data)
+        {
+            try
+            {
+                port.Write(data, 0, data.Length);
+                return true;
+            }
+            catch { SetStatus("COM Port Write Error/Abort"); }
+            return false;
+        }
+
+
+        public static SerialPort? OpenPort(string com)
+        {
+            SerialPort? port = null;
+            try
+            {
+                port = new SerialPort(com, 115200, Parity.None, 8, StopBits.One)
+                {
+                    ReadTimeout = 20000
+                };
+                port.Open();
+                return port;
+            }
+            catch { }
+            ClosePort(port);
+            return null;
+        }
+
+
+        public void Flash(string com, byte[] firmware)
+        {
+            int len = (int)Math.Ceiling(firmware.Length / 1024.0) * 1024;
+            if (OpenPort(com) is SerialPort port)
+            {
+                flashingPort = port;
+                try
+                {
+                    byte[] packet = ConstructPacket(0x39, 0x3305, [0x10], 0, 1);
+                    if (!WriteData(port, packet)) return;
+                    if (!GetAck(port)) return;
+                    if (!WriteData(port, packet)) return;
+                    if (!GetAck(port)) return;
+                    packet = ConstructPacket(0x39, 0x3305, [0x55], 0, 1);
+                    if (!WriteData(port, packet)) return;
+                    if (!GetAck(port)) return;
+                    for (int i = 0; i < len; i += 1024)
+                    {
+                        packet = ConstructPacket(0x57, i, firmware, i, 1024);
+                        if (!WriteData(port, packet)) return;
+                        if (!GetAck(port)) return;
+                        SetProgress(i, len - 1024);
+                    }
+                    if (!GetAck(port)) return;
+                    SetStatus("Firmware Flash Finished Okay");
+                }
+                finally { ClosePort(port); }
+            }
+            else
+                SetStatus($"Cannot open port: {com}");
+
+        }
+
+        public void SetProgress(int now, int len)
+        {
+            Invoke(() =>
+            {
+                double d = (double)now / len;
+                d *= 100;
+                if (d < 0) d = 0; else if (d > 100) d = 100;
+                ProgressBar.Value = (int)d;
+                SetStatus($"Progress: {(int)d}%");
+            });
+        }
+
+        public void SetStatus(string status)
+        {
+            Invoke(() =>
+            {
+                StatusLabel.Text = status;
+            });
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog ofd = new()
+            {
+                Title = "Open RT-880 Firmware Binary Image",
+                Filter = "BIN Files|*.bin|All Files|*.*"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StartButton.Enabled = true;
+                FileNameLabel.Text = ofd.FileName;
+            }
+        }
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            byte[] firmware;
+            try
+            {
+                firmware = File.ReadAllBytes(FileNameLabel.Text);
+            }
+            catch
+            {
+                SetStatus("Cannot read firmware bin file");
+                return;
+            }
+            BrowseButton.Enabled = false;
+            StartButton.Enabled = false;
+            AbortButton.Enabled = true;
+            ComPorts.Enabled = false;
+            string portName = ComPorts.SelectedItem?.ToString() ?? string.Empty;
+            using var task = Task.Run(() =>
+            {
+                Flash(portName, firmware);
+            });
+            await task;
+            BrowseButton.Enabled = true;
+            StartButton.Enabled = true;
+            AbortButton.Enabled = false;
+            ComPorts.Enabled = true;
+        }
+
+        private void ComPorts_DropDown(object sender, EventArgs e)
+        {
+            EnumerateSerialPorts();
+        }
+
+        private void AbortButton_Click(object sender, EventArgs e)
+        {
+            ClosePort(flashingPort);
+        }
+    }
+}
